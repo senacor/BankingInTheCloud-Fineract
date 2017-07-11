cdcb108362c5b2b8eed2613866e132809d74688c
Daniel <daniel.karzel@senacor.com>
2017-07-11 18:12:38 +0200

    added some console output to build script

--- fineract-project_snapshot/build_microservices.sh
+++ fineract-project_snapshot/build_microservices.sh
@@ -7,29 +7,34 @@ workingDir=$(pwd)
 # define the paths to all microservices (including all the core services and all the tools)
 # note that the order is of vital importance!
 # if there is an error during the build you may have to change the order (if a service is dependent on another then it will expect it in maven-local when building, thus it has to be built before)
-microservices-publish=("core/lang" "core/api" "core/async" "core/cassandra" "core/mariadb" "core/data-jpa" "core/command" "core/test" 
-               "tools/javamoney-lib" "tools/crypto" 
-               "anubis" 
-               "identity" 
-               "permitted-feign-client" 
-               "provisioner" 
-               "rhythm" 
-               "template" 
-               "office" 
-               "customer"
-               "group"
-               "accounting"
-               "portfolio"
-               "integration-tests/service-starter" "integration-tests/demo-server"
-               )
+declare -a microservicesPublish=(
+    "core/lang" "core/api" "core/async" "core/cassandra" "core/mariadb" "core/data-jpa" "core/command" "core/test" 
+    "tools/javamoney-lib" "tools/crypto" 
+    "anubis" 
+    "identity" 
+    "permitted-feign-client" 
+    "provisioner" 
+    "rhythm" 
+    "template" 
+    "office" 
+    "customer"
+    "group"
+    "accounting"
+    "portfolio"
+    "integration-tests/service-starter" "integration-tests/demo-server"
+)
 
-microservices-build-only=("integration-tests/test-provisioner-identity-organization" "integration-tests/test-accounting-portfolio")
+declare -a microservicesBuildOnly=(
+    "integration-tests/test-provisioner-identity-organization" "integration-tests/test-accounting-portfolio"
+)
 
 # build and publish to maven local
-for ms in "${microservices-publish[@]}"
+for ms in "${microservicesPublish[@]}"
 do
     # navigate to folder of service to build
     cd $ms
+
+    echo "::::: PUBLISHING: $ms"
 
     # special handling for javamoney-lib
     if [ "$ms" == "tools/javamoney-lib" ]
@@ -48,10 +53,12 @@ do
 done
 
 # build only services
-for ms in "${microservices-publish[@]}"
+for ms in "${microservicesBuildOnly[@]}"
 do
     # navigate to folder of service to build
     cd $ms
+
+    echo "::::: JUST-BUILD: $ms"
 
     chmod +x gradlew
     ./gradlew build
@@ -61,6 +68,7 @@ do
 done
 
 # fims-web-app
+echo "::::: FIMS-WEB-APP"
 cd fims-web-app
 npm i
 
