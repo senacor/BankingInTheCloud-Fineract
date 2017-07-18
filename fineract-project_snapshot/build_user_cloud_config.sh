dc2ee1204f98ba1b8fe6b795ebbcf09f7976a596
ttr <tuan-si.tran@senacor.com>
2017-07-18 12:30:14 +0200

    Preparing for automatic configuring of cloud services

---
+++ fineract-project_snapshot/build_user_cloud_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## generates a docker-compose.yml-file with correct settings, to work with cloud config server
+
+userName=$1
+appStage=$2
+microservices=("identity" "office" "customer" "accounting" "portfolio" "provisioner")
+
+if [ $userName == "" ]
+  then
+    userName="olaf"
+fi
+if [ $appStage == "" ]
+  then
+    appStage="dev"
+fi
+
+sed 's/<user>/'$userName'/g; s/<stage>/'$appStage'/g' docker-compose-skeleton.yml > docker-compose.yml
+
+for ms in "${microservices[@]}"
+do
+  cp "./config/"$ms"-v1-default.yml" "./config/"$ms"_"$userName"-v1-"$userName"-"$appStage".yml"
+done
